500b82daea1d48e3f3c276a1f57e954b4c9a1b7e
Rolando Guerra <rolandoguerra1991@gmail.com>
2022-10-13 22:30:29 -0400

    validado precio

--- src/app/template/basics/basics.component.ts
+++ src/app/template/basics/basics.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,13 +7,24 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./basics.component.css']
 })
 export class BasicsComponent implements OnInit {
+  @ViewChild('myForm') myForm!: NgForm;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  save(myForm: NgForm) {
-    console.log(myForm)
+  save() {
+    console.log(this.myForm)
+  }
+
+  nameValid():boolean {
+    return this.myForm?.controls['product']?.invalid
+      && this.myForm?.controls['product']?.touched
+  }
+
+  priceValid():boolean {
+    return this.myForm?.controls['price']?.value < 0
+      && this.myForm?.controls['price']?.touched;
   }
 }
